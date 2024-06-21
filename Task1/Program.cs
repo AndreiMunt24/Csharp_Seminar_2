ede02c9f1169b2990c39df12d253029d4d63b464
Andrei Munteanu <muntyanua@internet.ru>
2024-06-22 02:48:05 +0300

    zadacha1

---
+++ Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Введите число: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (IsDivisibleBy7And23(number))
+            {
+                Console.WriteLine($"Число {number} кратно одновременно 7 и 23.");
+            }
+            else
+            {
+                Console.WriteLine($"Число {number} не кратно одновременно 7 и 23.");
+            }
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Пожалуйста, введите корректное целое число.");
+        }
+    }
+
+    static bool IsDivisibleBy7And23(int number)
+    {
+        return number % 7 == 0 && number % 23 == 0;
+    }
+}
